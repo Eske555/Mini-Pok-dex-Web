bfaebaca8e0f328f5ed1f9cee2fa8f79bda19b54
IGNACIO ESKENAZI <49374119@ort.edu.ar>
2026-04-24 08:23:17 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,58 @@
+const input = document.getElementById("pokemonInput");
+const button = document.getElementById("searchBtn");
+const card = document.getElementById("pokemonCard");
+const loading = document.getElementById("loading");
+const error = document.getElementById("error");
+
+button.addEventListener("click", buscarPokemon);
+
+async function buscarPokemon() {
+  const nombre = input.value.toLowerCase().trim();
+
+  if (!nombre) {
+    mostrarError("Ingresá un nombre válido");
+    return;
+  }
+
+  limpiarUI();
+  loading.classList.remove("hidden");
+
+  try {
+    const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${nombre}`);
+
+    if (!res.ok) throw new Error("No encontrado");
+
+    const data = await res.json();
+    mostrarPokemon(data);
+
+  } catch (err) {
+    mostrarError("Pokémon no encontrado");
+  } finally {
+    loading.classList.add("hidden");
+  }
+}
+
+function mostrarPokemon(pokemon) {
+  const tipos = pokemon.types
+    .map(t => t.type.name)
+    .join(", ");
+
+    card.innerHTML = `
+    <h2>${pokemon.name}</h2>
+    <img src="${pokemon.sprites.front_default}">
+    <p>Tipo: ${tipos}</p>
+    <p>Peso: ${pokemon.weight}</p>
+    <p>Altura: ${pokemon.height}</p>
+  `;
+  card.classList.remove("hidden");
+}
+
+function mostrarError(msg) {
+  error.textContent = msg;
+  error.classList.remove("hidden");
+}
+function limpiarUI() {
+  card.innerHTML = "";
+  card.classList.add("hidden");
+  error.classList.add("hidden");
+}
